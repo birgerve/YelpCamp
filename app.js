f47cc9a7347b21be2a4f3fb7a3bd0544412105a8
Birger Van Eeckhoutte <birger.van.eeckhoutte@hotmail.be>
2018-11-26 16:06:51 +0000

    change app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ console.log(process.env.DATABASEURL);
 mongoose.set("useFindAndModify", false);
 //mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 //mongoose.connect('mongodb://birger:birger1@ds117334.mlab.com:17334/yelpcamp-birgerve', {useNewUrlParser: true});
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser:true});
+mongoose.connect(process.env.DATABASEURL);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
